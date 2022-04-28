5855bdf83d86e18a7250b8537b6da53db9443dcc
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-04-28 19:02:29 +0200

    Feat : add recipes galery

--- scripts/Api/fetch.js
+++
@@ -1,8 +0,0 @@
-// Fonction pour récuperer les données du json
-
-/** Récupération des recettes */
-async function getRecipes() {
-  return fetch("./data/recipes.json")
-    .then((file) => file.json())
-    .then((data) => data.recipes);
-}
