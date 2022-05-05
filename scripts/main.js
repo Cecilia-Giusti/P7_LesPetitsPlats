403afa3bc4e89466d199ece4da4060d5b7b3aeb5
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-05-05 13:41:40 +0200

    Feat : add change icon when click the button

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,16 @@ async function init() {
   // Création du tableau des recettes
   displayData(recipes);
 
-  //Recherche des recettes via la barre de recherche Algo 1
+  // BARRE DE RECHERCHE
+
+  //Recherche des recettes via la barre de recherche
   const researchInput = document.getElementById("searchBar");
   const researchForm = document.getElementById("searchForm");
 
   // Vider la barre de recherche lors du click
   researchInput.addEventListener("click", () => {
     researchInput.defaultValue = "";
-  }); // Don't - remettre une valeur lors d'un click ailleur
+  }); // Don't - remettre une valeur lors d'un click ailleurs
 
   // Evenement à l'envoi de la recherche
   researchForm.addEventListener("submit", (event) => {
@@ -38,6 +40,49 @@ async function init() {
       errorMessage("Veuillez entrer au minimum 3 lettres pour votre recherche");
     }
   });
+
+  // TAGS
+  const tagsInput = document.querySelectorAll(".tags__input");
+  const tagsBtn = document.querySelectorAll(".tags__button");
+
+  // Vider les champs  lors du click
+  tagsInput.forEach((input) =>
+    input.addEventListener("click", function () {
+      input.defaultValue = "";
+      console.log(input.id);
+    })
+  );
+
+  // Ouvrir les onglets lors du click sur la flèche
+  tagsBtn.forEach((button) =>
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log(button);
+
+      // Changement de l'icone
+      button.innerHTML = "";
+      arrow(button);
+    })
+  );
+
+  /**Fonction pour le changement d'icone en fonction de l'ouverture d'un tag
+   * @param {}
+   */
+  function arrow(button) {
+    if (button.getAttribute("class") != "tags__button open") {
+      const arrowUp = document.createElement("i");
+      arrowUp.setAttribute("class", "fi fi-bs-angle-up");
+      button.appendChild(arrowUp);
+      button.setAttribute("class", "tags__button open");
+    } else {
+      const arrowDown = document.createElement("i");
+      arrowDown.setAttribute("class", "fi fi-bs-angle-down ");
+      button.appendChild(arrowDown);
+      button.setAttribute("class", "tags__button");
+    }
+  }
+
+  // Fermer les onglets lors que click sur la flèche
 }
 
 init();
