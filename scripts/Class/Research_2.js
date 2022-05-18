51201dab183b9a09ff8bd5557ba011da8dd91d4f
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-05-18 09:44:17 +0200

    Docs: add investigation

--- scripts/Class/Research_2.js
+++ scripts/Class/Research_2.js
@@ -8,17 +8,39 @@ export class Research_2 {
     this._research = research.trim().toLowerCase();
   }
 
+  // /**Recherche des recettes correspondantes et ajout dans un tableau */
+  // researchSort() {
+  //   let recipesArray = this._recipes.filter((recipe) => {
+  //     let ingredientArray = recipe.ingredients.map((item) =>
+  //       item.ingredient.toLowerCase()
+  //     );
+  //     return (
+  //       recipe.name.toLowerCase().includes(this._research) ||
+  //       recipe.description.toLowerCase().includes(this._research) ||
+  //       ingredientArray.includes(this._research)
+  //     );
+  //   });
+  //   console.log(recipesArray);
+  //   return recipesArray;
+  // }
+
   /**Recherche des recettes correspondantes et ajout dans un tableau */
   researchSort() {
     let recipesArray = this._recipes.filter((recipe) => {
-      let ingredientArray = recipe.ingredients.map((item) =>
-        item.ingredient.toLowerCase()
-      );
-      return (
+      if (
         recipe.name.toLowerCase().includes(this._research) ||
-        recipe.description.toLowerCase().includes(this._research) ||
-        ingredientArray.includes(this._research)
-      );
+        recipe.description.toLowerCase().includes(this._research)
+      ) {
+        return (
+          recipe.name.toLowerCase().includes(this._research) ||
+          recipe.description.toLowerCase().includes(this._research)
+        );
+      } else {
+        let ingredientArray = recipe.ingredients.map((item) =>
+          item.ingredient.toLowerCase()
+        );
+        return ingredientArray.includes(this._research);
+      }
     });
     console.log(recipesArray);
     return recipesArray;
